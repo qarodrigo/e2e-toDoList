1864758b31bc598f505e61c4188a6f5d964ce6af
Rodrigo Santos <rodrigo.contato1@outlook.com>
2021-08-29 12:14:44 -0300

    some tests cases in to do list

---
+++ cypress/integration/flask.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+
+context('Testing to do list', () => {
+    beforeEach(() =>{
+        cy.visit(Cypress.env('host'))  
+        cy.get('.btn-primary').contains('Create a to-do list').click()
+    })
+
+    it('adding first task to a list', () => {
+    
+        cy.addTask()
+        
+        cy.contains('Take a shower').should('be.visible')
+    })
+
+
+    it('complete first task in list', () => {
+
+        cy.addTask()
+
+        cy.get('label[title="Done"]').click() // for dynamics selectors
+
+        cy.get('.done').contains('Take a shower').should('have.css', 'color', 'rgb(187, 187, 187)')
+    })
+
+    it.only('edit task in list', () => {
+
+        cy.addTask('Eat a pizza')
+        
+        cy.contains('Eat a pizza').type('Task edited')
+
+        cy.get('#task_description').click()
+                
+        cy.contains('Task edited').should('be.visible')
+    })
+
+    it('delete task in list', () => {
+        
+        cy.addTask()
+
+        cy.get('.delete-task').click()
+
+        cy.contains('Take a shower').should('not.exist')
+        
+    })
+
+})
